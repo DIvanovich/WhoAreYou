7ef6c90a9c18565a50abd94de9e528917d6d3f7c
Dmytro Shapoval <dmitro_@ukr.net>
2022-04-19 01:51:04 +0200

    add js test

--- js/js.js
+++ js/js.js
@@ -1,11 +1,36 @@
+// let select = document.querySelector('#pets');
+
+// select.onchange = function () {
+//     console.log(select.value);
+// }
+
+const {form} = document.forms;
+form.addEventListener('submit', () => {
+    event.preventDefault();
+    const {name, age, food, pet, drink, move} = form;
+    const Results = { 
+        name: name.value,
+        age: age.value,
+        food: food.value,
+        pet: pet.value, 
+        drink: drink.value, 
+        move: move.value
+    };
+    console.log(Results);
+})
 
 
-function to_eng (el) {
-	if(el.previousElementSibling.clientHeight === 80) {
-		el.previousElementSibling.style.height = "100%";
-		el.innerHTML = "ShowLess...";
-	} else {
-		el.previousElementSibling.style.height = "80px";
-		el.innerHTML = "Read More...";
-	}
-}
+
+// document.querySelector('form').addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     const form = document.querySelector('form');
+//     console.log(form);
+//     const name = form.elements.name.value
+// });
+
+// let testform = document.querySelector('#sendform');
+// testform.onclick = function(event) {
+//     event.preventDefault();
+//     console.log('work');
+// }
+

3ff049867f49892344fa903c1908fc77a2acd9d8
Dmytro Shapoval <dmitro_@ukr.net>
2022-04-21 17:29:08 +0200

    add jsscrips

--- js/js.js
+++ js/js.js
@@ -5,7 +5,7 @@
 // }
 
 const {form} = document.forms;
-form.addEventListener('submit', () => {
+form.addEventListener('submit', (event) => {
     event.preventDefault();
     const {name, age, food, pet, drink, move} = form;
     const Results = { 
@@ -17,16 +17,37 @@ form.addEventListener('submit', () => {
         move: move.value
     };
     console.log(Results);
-})
+}, { "passive": false })
+
+// let mainFormis = document.forms.mainForm;
+// console.log(mainFormis);
+// let mainFormRadioDrink = mainFormis.drink.value;
+// let mainFormInputName = mainFormis.name;
+// console.log(document.querySelector("form"));
+// console.log(mainFormRadioDrink);
 
 
 
-// document.querySelector('form').addEventListener('submit', (event) => {
-//     event.preventDefault();
-//     const form = document.querySelector('form');
-//     console.log(form);
-//     const name = form.elements.name.value
+// mainFormRadioDrink.addEventListener("change", function (e) {
+//        console.log(mainFormRadioDrink);
+//    });
+
+// mainFormInputName.addEventListener("input", function (event) {
+//     console.log('value: ${mainFormInputName.value}');
 // });
+
+
+// document.getElementById("#form-1").addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     console.log("ok");
+//     const form = document.getElementById("#form-1");
+//     console.dir(form);
+//     const name = form.elements.name.value
+//     form.reset();
+    
+// });
+
+
 
 // let testform = document.querySelector('#sendform');
 // testform.onclick = function(event) {
